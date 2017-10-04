09ec67b1f161a38e7fca4df8bd6eb7482a68e579
AlanPi1992 <xpi@andrew.cmu.edu>
2017-10-04 12:48:46 -0400

    add multicalss.h

---
+++ src/minami/multiclass.h
@@ -0,0 +1,63 @@
+#ifndef MULTICLASS_H
+#define MULTICLASS_H
+
+#include "vehicle.h"
+#include "utls.h"
+#include "dlink.h"
+
+#include <vector>
+#include <deque>
+#include <unordered_map>
+
+class MNM_Veh;
+class MNM_Dnode;
+class MNM_Dlink;
+
+/**************************************************************************
+							Multiclass CTM
+**************************************************************************/
+class MNM_Dlink_Ctm_Multiclass : public MNM_Dlink
+{
+	MNM_Dlink_Ctm_Multiclass(TInt ID,
+							 TFlt lane_hold_cap,
+							 TFlt lane_flow_cap,
+							 TInt number_of_lane,
+							 TFlt length,
+							 TFlt ffs,
+							 TFlt unit_time,
+							 TFlt flow_scalar);
+	~MNM_Dlink_Ctm_Multiclass();
+	int virtual evolve(TInt timestamp) override;
+	TFlt virtual get_link_supply() override;
+	int virtual clear_incoming_array() override;
+	void virtual print_info() override;
+	TFlt virtual get_link_flow() override;
+	TFlt virtual get_link_tt() override;
+
+	class Ctm_Cell_Multiclass;
+	int init_cell_array(Tflt unit_time, TFlt std_cell_length, TFlt lane_hold_cap_last_cell);
+	int update_out_vehicle();
+	int move_last_cell();
+	TInt m_num_cells;
+	TFlt m_lane_hold_cap;
+	TFlt m_lane_flow_cap;
+	TFlt m_flow_scalar;
+	TFlt m_wave_ratio;
+	TFlt m_last_wave_ratio;
+	std::vector<Ctm_Cell*> m_cell_array;
+};
+
+class MNM_Dlink_Ctm_Multiclass::Ctm_Cell_Multiclass
+{
+	Ctm_Cell_Multiclass(TFlt hold_cap, TFlt flow_cap, TFlt flow_scalar, TFlt wave_ratio);
+	~Ctm_Cell_Multiclass();
+	TFlt get_demand();
+	TFlt get_supply();
+
+	TFlt m_volume;
+	TFlt m_flow_scalar;
+	TFlt m_hold_cap;
+	TFlt m_wave_ratio;
+	TFlt m_out_veh;
+	std::deque<MNM_Veh*> m_veh_queue;
+}
