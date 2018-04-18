9645d05bcd0e90d922b86c0c0aa904bccecaaa33
Alan-ubuntu <xpi@andrew.cmu.edu>
2018-04-17 23:33:06 -0700

    bug fixed running on SPC network

--- src/examples/test_multiclass.cpp
+++ src/examples/test_multiclass.cpp
@@ -29,9 +29,10 @@ int main()
 	printf("========================== Finished pre_loading! ==========================\n");
 
 	printf("\n\n\n====================================== Start loading! =======================================\n");
+	bool _verbose = false;
 	while (!test_dta -> finished_loading(_current_inter)){
 		printf("Current interval: %d\n", _current_inter());
-		test_dta -> load_once(true, _current_inter, _assign_inter);
+		test_dta -> load_once(_verbose, _current_inter, _assign_inter);
 		if (_current_inter % test_dta -> m_assign_freq == 0 || _current_inter == 0){
 			_assign_inter += 1;
 		}
@@ -41,24 +42,24 @@ int main()
 	}
 
 
-	TInt _iter = 0;
-	while (_iter < _current_inter){
-		printf("---------------------------------- Current interval: %d ----------------------------------\n", int(_iter));
-		for (auto _link_it = test_dta -> m_link_factory -> m_link_map.begin(); 
-				  _link_it != test_dta -> m_link_factory -> m_link_map.end(); _link_it++){
-			_link = _link_it -> second;
-			printf("link ID is %d: ", _link -> m_link_ID());
-			_link_m = dynamic_cast<MNM_Dlink_Multiclass*>(_link);
-			printf("car flow(%.4f), ", double(MNM_DTA_GRADIENT::get_link_inflow_car(_link_m, _iter, _iter + 1)));
-			printf("truck flow(%.4f), ", double(MNM_DTA_GRADIENT::get_link_inflow_truck(_link_m, _iter, _iter + 1)));
-			printf("car tt(%.4f s), ", double(MNM_DTA_GRADIENT::get_travel_time_car(_link_m, TFlt(_iter + 1))));
-			printf("truck tt(%.4f s), ", double(MNM_DTA_GRADIENT::get_travel_time_truck(_link_m, TFlt(_iter + 1))));
-			printf("car tt(%.4f s), ", double(_link_m -> get_link_freeflow_tt_car()));
-			printf("truck tt(%.4f s)\n", double(_link_m -> get_link_freeflow_tt_truck()));
-		}
-		_iter += 1;
-		// if (_iter > 100){ break; };
-	}
+	// TInt _iter = 0;
+	// while (_iter < _current_inter){
+	// 	printf("---------------------------------- Current interval: %d ----------------------------------\n", int(_iter));
+	// 	for (auto _link_it = test_dta -> m_link_factory -> m_link_map.begin(); 
+	// 			  _link_it != test_dta -> m_link_factory -> m_link_map.end(); _link_it++){
+	// 		_link = _link_it -> second;
+	// 		printf("link ID is %d: ", _link -> m_link_ID());
+	// 		_link_m = dynamic_cast<MNM_Dlink_Multiclass*>(_link);
+	// 		printf("car flow(%.4f), ", double(MNM_DTA_GRADIENT::get_link_inflow_car(_link_m, _iter, _iter + 1)));
+	// 		printf("truck flow(%.4f), ", double(MNM_DTA_GRADIENT::get_link_inflow_truck(_link_m, _iter, _iter + 1)));
+	// 		printf("car tt(%.4f s), ", double(MNM_DTA_GRADIENT::get_travel_time_car(_link_m, TFlt(_iter + 1))));
+	// 		printf("truck tt(%.4f s), ", double(MNM_DTA_GRADIENT::get_travel_time_truck(_link_m, TFlt(_iter + 1))));
+	// 		printf("car tt(%.4f s), ", double(_link_m -> get_link_freeflow_tt_car()));
+	// 		printf("truck tt(%.4f s)\n", double(_link_m -> get_link_freeflow_tt_truck()));
+	// 	}
+	// 	_iter += 1;
+	// 	// if (_iter > 100){ break; };
+	// }
 
 	
 	// for (auto _link_it = test_dta -> m_link_factory -> m_link_map.begin(); _link_it != test_dta -> m_link_factory -> m_link_map.end(); _link_it++){
